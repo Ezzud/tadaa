695d8c469bbaf4dbbc8f36dc00877fd092cbcd1d
Ezzud <44119886+Ezzud@users.noreply.github.com>
2021-08-25 02:37:57 +0200

    Delete invalidated.js

--- events/invalidated.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const Discord = require("discord.js");
-const settings = require('../config.json');
-module.exports = async (client) => {
-    await client.destroy()
-    await client.login(settings.token)
-}
